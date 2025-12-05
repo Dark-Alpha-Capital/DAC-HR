2e5b6d8db93cb852dbd60d8658ce54681ff80ab9
Rahulg321 <rg5353070@gmail.com>
2025-12-05 23:57:48 +0530

    Add console log to RootLayout and clean up component files
    - Added a console log statement in the RootLayout component for debugging purposes.
    - Removed trailing whitespace from several filter components and the EmployeesListSkeleton component for improved code cleanliness.

--- apps/web/app/(main-site)/layout.tsx
+++ apps/web/app/(main-site)/layout.tsx
@@ -25,6 +25,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  console.log("RootLayout");
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
