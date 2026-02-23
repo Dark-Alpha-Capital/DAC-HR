3b34303f915e57a8c957562182eb93665bd036ce
Rahulg321 <rg5353070@gmail.com>
2026-02-23 23:15:22 +0530

    Add console log to Page component for debugging

--- apps/web/app/(main-site)/page.tsx
+++ apps/web/app/(main-site)/page.tsx
@@ -31,6 +31,8 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
+  console.log("Page");
+
   return (
     <Suspense fallback={<HomeLoading />}>
       <UserContent />
